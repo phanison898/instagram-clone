46b83fa4fe4721fa3da6dd372ce4473c7c90656b
phanison898 <phanison898@gmail.com>
2021-03-13 06:47:21 +0530

    fixed story name overflow issue

--- src/components/stories/Stories.js
+++ src/components/stories/Stories.js
@@ -6,38 +6,21 @@ import Style from "./Style";
 
 const Stories = () => {
   const classes = Style();
-  const { users } = useSelector((state) => state.users);
+  const { users: allUsers } = useSelector((state) => state.users);
 
-  const [_users, setUsers] = useState([]);
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    setUsers(users);
-  }, []);
+    setUsers(allUsers);
+  }, [allUsers]);
 
   return (
     <Paper className={classes.stories}>
-      {_users.map(({ uid, profilePic, fullName }, i) => (
+      {users.map(({ uid, profilePic, fullName }, i) => (
         <Story key={uid} profileImage={profilePic} title={fullName} />
       ))}
     </Paper>
   );
 };
-
-const usernames = [
-  "Walter",
-  "Jessy",
-  "Hank",
-  "Skinny",
-  "Badger",
-  "Mike",
-  "Gus",
-  "Hector",
-  "Toco",
-  "Tommy",
-  "Arthur",
-  "John",
-  "Finn",
-  "Alfie",
-];
 
 export default Stories;
--- src/components/stories/Story.js
+++ src/components/stories/Story.js
@@ -4,10 +4,15 @@ import Style from "./Style";
 
 function Story({ profileImage, title }) {
   const classes = Style();
+
+  const shrinkText = (text) => {
+    return text.length > 8 ? text.substring(0, 6) + " ..." : text;
+  };
+
   return (
     <div className={classes.story}>
       <Avatar src={profileImage} className={classes.profilePic} />
-      <p>{title}</p>
+      <p>{shrinkText(title)}</p>
     </div>
   );
 }
